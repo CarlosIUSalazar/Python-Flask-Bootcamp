ee8d41406f6e4ce0bf145f26de5d8794dcf3bc72
Carlos Salazar <carlosiusalazar@gmail.com>
2020-09-06 18:59:03 +0900

    Flask Flash messages Finished project

--- Flask-Forms-Flash/basic.py
+++ Flask-Forms-Flash/basic.py
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = 'kmykey'
 
 class SimpleForm(FlaskForm):
 
+    breed = StringField('What breed are you?')
     submit = SubmitField('Click me')
 
 @app.route('/',methods=['GET','POST'])
@@ -16,7 +17,8 @@ def index():
     form = SimpleForm()
 
     if form.validate_on_submit():
-        flash('You just clicked the button!') #this causes an object called get_flashed_messages() to be available in index.html
+        session['breed'] = form.breed.data
+        flash(f"You just changed your breed to {session['breed']}") #this causes an object called get_flashed_messages() to be available in index.html
         return redirect(url_for('index')) #We are serving the same page again
 
     return render_template('index.html', form=form)
