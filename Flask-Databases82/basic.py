c7f94011b67650b569869e1708fc9a71359821ed
Carlos Salazar <carlosiusalazar@gmail.com>
2020-09-20 12:26:02 +0900

    Studrying SQLAlchemy - Migrations

--- Flask-Databases82/basic.py
+++ Flask-Databases82/basic.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate #pip install Flask-Migrate
 
 ## Create a SQLite DB ##
 basedir = os.path.abspath(os.path.dirname(__file__))  #This file variable is set to the name of the actual file.  Is set to basic.py essentially.  And then we want to get the directory name of basic.py   And then you want the absolute path of this file and directory name.
@@ -14,6 +15,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ## End Creation of DB
 
+migrate = Migrate(app,db) #Connect our app with db.  
+
+#remember to use export FLASK_APP=basic.py
+# then flask db init
+# flask db migrate -m "create puppy table"    This creates a migration file and this way you can leave comments for each migration
+# flask db upgrade   This finally apply the migration to the database
+
 #### DB Model Description ###
 class Puppy(db.Model):
 
@@ -22,15 +30,19 @@ class Puppy(db.Model):
     id = db.Column(db.Integer,primary_key=True)  #is is a column that is primary key as well.
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)    #this column was added after the first db init so we need a migratio se below **
+
 
     def __init__(self,name,age):     #Id is auto passed so no need to type it in here
         self.name = name
         self.age = age
+        self.breed = breed #this column was added after the first db init so we need a migratio se below **
 
     def __repr__(self):  #Gives you the string representation of the object, so you can print it out.
         return f"Puppy {self.name} is {self.age} years old"
 
 
-
+# ** flask db migrate -m "Added breed column"
+#    flask db upgrade
 
 
